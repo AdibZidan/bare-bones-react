29f3dd5dcd8f9305029c0890f130e1a945c08335
Adib Zidan <94Amsz@gmail.com>
2022-09-18 18:23:50 +0200

    Update jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.paths.json');
+
 export default {
-  clearMocks: true
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+    '\\.scss$': '<rootDir>/scssTransformer.js'
+  },
+  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/src/**/*.test.tsx'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  })
 };
